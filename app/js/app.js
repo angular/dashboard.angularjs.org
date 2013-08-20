950581959b88c3805978f8b8082f01117f5623e9
Igor Minar <igor@angularjs.org>
2013-08-19 17:01:37 -0700

    update google3 sync status + add v1_0 branch
    this code is super messy because of the existing app structure
    
    I couldn't take it any more and at least refactored the sync widget into
    a component directive.

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('ngDashboard', ['ngDashboard.filters', 'ngDashboard.services']).
+angular.module('ngDashboard', ['ngDashboard.filters', 'ngDashboard.services', 'ngDashboard.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/twitter.html', controller: TwitterCtrl});
     $routeProvider.when('/view2', {templateUrl: 'partials/gplus.html', controller: GPlusCtrl});
