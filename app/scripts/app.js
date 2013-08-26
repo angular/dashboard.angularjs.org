96da59280cc811d5767e622ca3c8b190874a6db6
Misko Hevery <misko@hevery.com>
2013-08-26 13:23:45 -0700

    GitHub service implementation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
-angular.module('dashboardApp', []);
+angular
+    .module('dashboardApp', ['github'])
+    .controller('GitHubTestController', GitHubTestController);
+
+
+function GitHubTestController(gitHub) {
+  this.shasSinceMasterRelease = gitHub.getSHAsSinceSinceRelease('master');
+  this.shasSinceStableRelease = gitHub.getSHAsSinceSinceRelease('v1.0.x');
+
+  this.g3MasterBehind = gitHub.getSHAsSince('master', 'g3_v1_x');
+  this.g3StableBehind = gitHub.getSHAsSince('v1.0.x', 'g3_v1_0');
+}
---
+++ app/scripts/services/github.js
@@ -0,0 +1,58 @@
+'use strict';
+
+angular
+    .module('github', [])
+    .value('gitHubAuth', {
+      client_id: localStorage.getItem('github.client_id'),
+      client_secret: localStorage.getItem('github.client_secret')
+    })
+    .service('gitHub', GitHub);
+
+function GitHub(gitHubAuth, $http) {
+  var url = 'https://api.github.com/repos/angular/angular.js';
+  var self = this;
+
+  this.getTags = function() {
+    return $http.
+        get(url + '/tags', {
+          params: gitHubAuth
+        }).
+        then(function(response) {
+          return response.data;
+        });
+  };
+
+  this.getLatestTagStartingWith = function(startingWith) {
+    return this.getTags().then(function(tags) {
+      for (var i = 0, ii = tags.length; i < ii; i++) {
+        var tag = tags[i];
+        if (tag.name.indexOf(startingWith) == 0) {
+          return tag.name;
+        }
+      }
+      return null;
+    });
+  };
+
+  this.getSHAsSince = function(branchName, sinceTag) {
+    return $http.
+        get(url + '/compare/' + sinceTag + '...' + branchName, {
+          params: gitHubAuth
+        }).
+        then(function(response) {
+          return response.data.ahead_by;
+        });
+  };
+
+  this.getSHAsSinceSinceRelease = function(branchName) {
+    var startingWithTag = (branchName == 'master')
+        ? 'v1.2'
+        : branchName.replace(/\.x$/, '');
+    return this.
+        getLatestTagStartingWith(startingWithTag).
+        then(function(latestTag) {
+          return self.getSHAsSince(branchName, latestTag);
+        });
+  }
+}
+
