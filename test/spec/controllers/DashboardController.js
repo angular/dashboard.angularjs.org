740e9347df3bbc57312c527d78d6341b4b8ef443
Igor Minar <igor@angularjs.org>
2013-08-26 14:02:34 -0700

    fix dashboardcontroller test

--- test/spec/controllers/DashboardController.js
+++ test/spec/controllers/DashboardController.js
@@ -3,20 +3,48 @@
 describe('Controller: DashboardcontrollerCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('dashboard.angularjs.orgApp'));
+  beforeEach(module('dashboardApp'));
 
-  var DashboardcontrollerCtrl,
-    scope;
+  var dashboardController,
+      scope;
 
   // Initialize the controller and a mock scope
+  beforeEach(module(function($provide) {
+    $provide.decorator('schedule', function($delegate, $timeout) {
+      $delegate.flush = function() {
+        $timeout.flush(60*1000);
+      };
+
+      return $delegate;
+    });
+
+
+    $provide.factory('jenkins', function($q) {
+      return {
+        buildStatus: function() {
+          var deferred = $q.defer();
+          deferred.resolve({
+            happy: true,
+            since: 1111
+          });
+
+          return deferred.promise;
+        }
+      };
+    });
+  }));
+
+
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    DashboardcontrollerCtrl = $controller('DashboardcontrollerCtrl', {
+    dashboardController = $controller('DashboardController', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+  it('should initialize buildStatus model', inject(function(schedule) {
+    expect(dashboardController.buildStatus).toBeUndefined();
+    schedule.flush();
+    expect(dashboardController.buildStatus).toEqual({happy: true, since: 1111});
+  }));
 });
