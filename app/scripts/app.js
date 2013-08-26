143fae6a568533dd2db8f965f06e7f78806041d7
Igor Minar <igor@angularjs.org>
2013-08-26 10:38:54 -0700

    deleted unused code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,3 @@
 'use strict';
 
-angular.module('dashboard.angularjs.orgApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('dashboardApp', []);
