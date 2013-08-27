f18dca1676f7f87853dd59ce659009fe42449eab
Vojta Jina <vojta.jina@gmail.com>
2013-08-26 22:03:57 -0700

    Wire untriaged PRs/issues

--- app/scripts/controllers/GithubStatusController.js
+++ app/scripts/controllers/GithubStatusController.js
@@ -1,19 +1,35 @@
 'use strict';
 
+var GithubCard = function(title, classes) {
+  CardViewData.call(this, title, null, null, ['github-card'].join(classes));
+};
 
-app.controller('GithubStatusController', function GithubStatusController($scope) {
+GithubCard.prototype.update = function(count, total) {
+  this.content = count;
+
+  if (angular.isDefined(total)) {
+    this.note = 'out of ' + total;
+  }
+};
+
+app.controller('GithubStatusController', function GithubStatusController($scope, schedule, github) {
+  var milestonePRsCard = new GithubCard('Pull requests', ['pr-card']);
+  var milestoneIssuesCard = new GithubCard('Issues', ['issue-card']);
+  var untriagedPRsCard = new GithubCard('Untriaged _Pull requests_', ['untriaged-card', 'pr-card', 'untriaged-pr-card']);
+  var untriagedIssuesCard = new GithubCard('Untriaged _Issues_', ['untriaged-card', 'issue-card', 'untriaged-issue-card']);
+  var totalPRsCard = new GithubCard('Total _Pull requests_', ['total-card', 'pr-card', 'total-pr-card']);
+  var totalIssuesCard = new GithubCard('Total _Issues_', ['total-card', 'issue-card', 'total-issue-card']);
+
   $scope.milestone = {
     title: '1.2.0-RC2',
-    cards: [
-      new CardViewData('Pull requests', '5', 'out of 41', ['github-card', 'pr-card']),
-      new CardViewData('Issues', '48', 'out of 230', ['github-card', 'issue-card']),
-      new CardViewData('Untriaged _Pull requests_', '2', null, ['github-card', 'untriaged-card', 'pr-card', 'untriaged-pr-card']),
-      new CardViewData('Untriaged _Issues_', '31', null, ['github-card', 'untriaged-card', 'issue-card', 'untriaged-issue-card'])
-    ],
-
-    totalCards: [
-      new CardViewData('Total _Pull requests_', '81', null, ['github-card', 'total-card', 'pr-card', 'total-pr-card']),
-      new CardViewData('Total _Issues_', '420', null, ['github-card', 'total-card', 'issue-card', 'total-issue-card'])
-    ]
+    cards: [milestonePRsCard, milestoneIssuesCard, untriagedPRsCard, untriagedIssuesCard],
+    totalCards: [totalPRsCard, totalIssuesCard]
   };
+
+  schedule.onceAMinute(function() {
+    github.getUntriagedCounts().then(function(count) {
+      untriagedPRsCard.update(count.prs);
+      untriagedIssuesCard.update(count.issues);
+    });
+  });
 });
