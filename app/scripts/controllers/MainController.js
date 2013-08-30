aabdd1f9cdac579e895a2d681c54334b03d4828a
jankuca <jan@jankuca.com>
2013-08-29 19:32:52 -0700

    fix branch names in broken build notifications

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -7,7 +7,7 @@ function MainController($scope) {
   $scope.$on('dash:buildUpdate', function (e, branch, buildStatus) {
     if (buildStatus.happy && (!$scope.brokenBuild || $scope.brokenBuild.branch === branch)) {
       $scope.brokenBuild = null;
-    } else {
+    } else if (!buildStatus.happy) {
       $scope.brokenBuild = {
         branch: branch,
         since: buildStatus.since
