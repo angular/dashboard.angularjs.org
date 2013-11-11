7db72a84cd525dd68438cc3d9ccaa53eb58ba684
Jeff Cross <middlefloor@gmail.com>
2013-11-11 10:55:25 -0800

    bump(version): bumped js version to 1.2.1

--- app/scripts/services/configService.js
+++ app/scripts/services/configService.js
@@ -3,8 +3,8 @@
 var angularJsConfig = {
   githubProject: 'angular.js',
   nextMilestone: {
-    title: 'v1.2.0',
-    githubName:  '1.2.0'
+    title: 'v1.2.1',
+    githubName:  '1.2.1'
   },
   branches: {
     master: {
