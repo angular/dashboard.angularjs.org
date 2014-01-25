56413fbe9512e48208d5b553734174685f43bf96
Brian Ford <btford@umich.edu>
2014-01-24 19:56:47 -0800

    update(milestone): bump milestone to 1.2.11

--- app/scripts/services/configService.js
+++ app/scripts/services/configService.js
@@ -3,8 +3,8 @@
 var angularJsConfig = {
   githubProject: 'angular.js',
   nextMilestone: {
-    title: 'v1.2.10',
-    githubName:  '1.2.10'
+    title: 'v1.2.11',
+    githubName:  '1.2.11'
   },
   branches: {
     master: {
