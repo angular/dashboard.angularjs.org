0da2a2fa71ff302c37ed98afd0ca3dd1523badfa
James deBoer <james@huronbox.com>
2014-02-10 11:53:55 -0800

    Dart to 0.9.7

--- app/scripts/services/configService.js
+++ app/scripts/services/configService.js
@@ -27,8 +27,8 @@ var angularJsConfig = {
 var angularDartConfig = {
   githubProject: 'angular.dart',
   nextMilestone: {
-    title: 'v0.9.7',
-    githubName:  'v0.9.7'
+    title: 'v0.9.8',
+    githubName:  'v0.9.8'
   },
   branches: {
     master: {
