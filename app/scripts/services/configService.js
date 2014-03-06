3fcd9a897e7bdbd6288c64191b049261d27ade2f
Brian Ford <btford@umich.edu>
2014-03-06 11:25:18 -0800

    chore: update stable and g3 refs for AngularJS

--- app/scripts/services/configService.js
+++ app/scripts/services/configService.js
@@ -10,16 +10,16 @@ var angularJsConfig = {
     master: {
       title: 'master',
       name: 'master',
-      g3Name: 'g3_v1_2',
-      releaseTag: 'v1.2',
+      g3Name: 'g3_v1_3',
+      releaseTag: 'v1.3',
       jenkinsProjectId: 'angular.js-angular-master'
     },
     stable: {
-      title: 'stable/1.0',
-      name: 'v1.0.x',
-      g3Name: 'g3_v1_0',
-      releaseTag: 'v1.0',
-      jenkinsProjectId: 'angular.js-angular-v1.0.x'
+      title: 'stable/1.2',
+      name: 'v1.2.x',
+      g3Name: 'g3_v1_2',
+      releaseTag: 'v1.2',
+      jenkinsProjectId: 'angular.js-angular-v1.2.x'
     }
   }
 };
